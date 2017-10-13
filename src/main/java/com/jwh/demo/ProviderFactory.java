9dd9f07c7a9a9a91c4636654d2ef6717e2caed46
蒋文桦 <wenhua.jiang@eku001.com>
2017-10-13 18:42:42 +0800

    add zk register center

--- src/main/java/com/jwh/demo/ProviderFactory.java
+++ src/main/java/com/jwh/demo/ProviderFactory.java
@@ -2,6 +2,7 @@ package com.jwh.demo;
 
 
 import com.jwh.demo.annotation.RpcProvider;
+import com.jwh.demo.zk.ServerZookeeper;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -14,9 +15,24 @@ import java.util.Set;
  */
 public class ProviderFactory implements ApplicationContextAware{
 
+    private String zkUrl;
+    private Integer timeout;
+    private String localAddress;
     private Integer port;
 
     private static ApplicationContext applicationContext;
+
+    public void setZkUrl(String zkUrl) {
+        this.zkUrl = zkUrl;
+    }
+
+    public void setTimeout(Integer timeout) {
+        this.timeout = timeout;
+    }
+
+    public void setLocalAddress(String localAddress) {
+        this.localAddress = localAddress;
+    }
 
     public void setPort(Integer port) {
         this.port = port;
@@ -40,6 +56,15 @@ public class ProviderFactory implements ApplicationContextAware{
             RpcProvider rpcProvider = obj.getClass().getAnnotation(RpcProvider.class);
             if(rpcProvider != null){
                 services.add(obj);
+                //注册服务到zk
+                try{
+                    ServerZookeeper.zkUrl = zkUrl;
+                    ServerZookeeper.timeout = timeout;
+                    ServerZookeeper.zkConnect();
+                    ServerZookeeper.createNode(localAddress,port,obj.getClass().getName());
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
             }
         }
         //新开线程，避免阻塞spring容器启动；
